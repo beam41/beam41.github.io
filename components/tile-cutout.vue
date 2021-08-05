d37194cc4283d6a2b7d67179d3812a40f5402222
Meehoi <beam_baimon@hotmail.com>
2021-08-05 10:31:24 +0700

    clean up cutout css

--- components/tile-cutout.vue
+++ components/tile-cutout.vue
@@ -10,20 +10,14 @@
   height: 50px;
   overflow: hidden;
   display: flex;
-  &:before, &:after {
-    content: '';
-    width: 8px;
-    background: white
-  }
+  border: 8px solid white;
 
   .cutout-inner {
-    box-sizing: border-box;
-    height: calc(100% + 16px);
+    height: 100%;
     width: 100%;
-    border: 8px solid white;
-    border-radius: 20px;
-    margin: -8px;
-
+    border: 16px solid white;
+    border-radius: 30px;
+    margin: -16px;
   }
 }
 </style>
