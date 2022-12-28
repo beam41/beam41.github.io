c9c94a436c584d9cea2b57a1f3221a60299f63ca
Meehoi <beam_baimon@hotmail.com>
2022-12-28 18:23:24 +0700

    add meta info

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,23 @@ export default defineNuxtConfig({
     typeCheck: true,
     strict: true,
   },
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'en',
+      },
+      title: 'Phumdol',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          name: 'description',
+          content: 'Phumdol Profile Website',
+        },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+  },
   css: ['normalize.css/normalize.css', '@/assets/css/index.scss'],
   modules: [
     [
