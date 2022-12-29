7f936a867c1901a2dcf506ee82230a8f5503efd3
Meehoi <beam_baimon@hotmail.com>
2022-12-30 00:02:29 +0700

    remove test log from plugin

--- plugins/vue-gtag.client.ts
+++ plugins/vue-gtag.client.ts
@@ -1,7 +1,6 @@
 import VueGtag from 'vue-gtag-next'
 
 export default defineNuxtPlugin((nuxtApp) => {
-  console.log(process.env.NODE_ENV)
   nuxtApp.vueApp.use(VueGtag, {
     property: {
       id: 'G-5KNT5HZ7PT',
