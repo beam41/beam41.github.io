7b82785036d3325cebd4eb536ccde9288fbb0fb5
Meehoi <beam_baimon@hotmail.com>
2022-12-29 23:58:31 +0700

    remove process.dev from plugin

--- plugins/vue-gtag.client.ts
+++ plugins/vue-gtag.client.ts
@@ -1,9 +1,11 @@
 import VueGtag from 'vue-gtag-next'
 
 export default defineNuxtPlugin((nuxtApp) => {
+  console.log(process.env.NODE_ENV)
   nuxtApp.vueApp.use(VueGtag, {
     property: {
       id: 'G-5KNT5HZ7PT',
     },
+    isEnabled: process.env.NODE_ENV !== 'development',
   })
 })
